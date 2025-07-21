5bf250d869af1aaa0d88d99309d0b5fa12b9c2fe
Keber Flores <keberflores@gmail.com>
2025-07-21 06:39:37 -0400

    refactor: changed browser to Chrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,11 @@ import org.example.drivers.DriverSingleton;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.sql.SQLOutput;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        DriverSingleton driverSingleton = DriverSingleton.getInstance("Firefox");
+        
+        DriverSingleton driverSingleton = DriverSingleton.getInstance("Chrome");
         WebDriver driver = driverSingleton.getDriver();
 
         driver.get("https://www.wikipedia.org/");
